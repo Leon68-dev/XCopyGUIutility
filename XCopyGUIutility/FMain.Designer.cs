3ba9e5d33db77db2e36d53c955135b6b78dad1e8
Leon68-dev <leon-dev@ukr.net>
2025-07-26 12:28:41 +0300

    changing (cmd)

--- XCopyGUIutility/FMain.Designer.cs
+++ XCopyGUIutility/FMain.Designer.cs
@@ -41,10 +41,10 @@
             groupBox2 = new GroupBox();
             chkI = new CheckBox();
             chkH = new CheckBox();
+            chkDate = new CheckBox();
             chkY = new CheckBox();
             chkE = new CheckBox();
             chkS = new CheckBox();
-            chkDate = new CheckBox();
             groupBox3 = new GroupBox();
             dtpDate = new DateTimePicker();
             chkJ = new CheckBox();
@@ -59,7 +59,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 15);
+            label1.Location = new Point(6, 12);
             label1.Name = "label1";
             label1.Size = new Size(54, 20);
             label1.TabIndex = 0;
@@ -67,22 +67,22 @@
             // 
             // txtSource
             // 
-            txtSource.Location = new Point(102, 12);
+            txtSource.Location = new Point(96, 9);
             txtSource.Name = "txtSource";
-            txtSource.Size = new Size(637, 27);
+            txtSource.Size = new Size(576, 27);
             txtSource.TabIndex = 1;
             // 
             // txtDestination
             // 
-            txtDestination.Location = new Point(102, 45);
+            txtDestination.Location = new Point(96, 42);
             txtDestination.Name = "txtDestination";
-            txtDestination.Size = new Size(637, 27);
+            txtDestination.Size = new Size(577, 27);
             txtDestination.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 48);
+            label2.Location = new Point(6, 45);
             label2.Name = "label2";
             label2.Size = new Size(85, 20);
             label2.TabIndex = 2;
@@ -90,7 +90,7 @@
             // 
             // btnBrowseSource
             // 
-            btnBrowseSource.Location = new Point(745, 11);
+            btnBrowseSource.Location = new Point(678, 8);
             btnBrowseSource.Name = "btnBrowseSource";
             btnBrowseSource.Size = new Size(97, 29);
             btnBrowseSource.TabIndex = 4;
@@ -100,7 +100,7 @@
             // 
             // btnBrowseDestination
             // 
-            btnBrowseDestination.Location = new Point(745, 44);
+            btnBrowseDestination.Location = new Point(678, 41);
             btnBrowseDestination.Name = "btnBrowseDestination";
             btnBrowseDestination.Size = new Size(97, 29);
             btnBrowseDestination.TabIndex = 5;
@@ -111,7 +111,7 @@
             // btnCopy
             // 
             btnCopy.BackColor = SystemColors.ActiveCaption;
-            btnCopy.Location = new Point(748, 225);
+            btnCopy.Location = new Point(581, 255);
             btnCopy.Name = "btnCopy";
             btnCopy.Size = new Size(94, 29);
             btnCopy.TabIndex = 7;
@@ -123,17 +123,17 @@
             // 
             txtOutput.BackColor = SystemColors.ActiveCaption;
             txtOutput.ForeColor = SystemColors.WindowText;
-            txtOutput.Location = new Point(12, 294);
+            txtOutput.Location = new Point(6, 286);
             txtOutput.Multiline = true;
             txtOutput.Name = "txtOutput";
-            txtOutput.Size = new Size(830, 265);
+            txtOutput.Size = new Size(769, 260);
             txtOutput.TabIndex = 8;
             // 
             // btnStop
             // 
             btnStop.BackColor = SystemColors.ActiveCaption;
             btnStop.Enabled = false;
-            btnStop.Location = new Point(748, 257);
+            btnStop.Location = new Point(681, 255);
             btnStop.Name = "btnStop";
             btnStop.Size = new Size(94, 29);
             btnStop.TabIndex = 9;
@@ -145,12 +145,13 @@
             // 
             groupBox2.Controls.Add(chkI);
             groupBox2.Controls.Add(chkH);
+            groupBox2.Controls.Add(chkDate);
             groupBox2.Controls.Add(chkY);
             groupBox2.Controls.Add(chkE);
             groupBox2.Controls.Add(chkS);
-            groupBox2.Location = new Point(14, 81);
+            groupBox2.Location = new Point(8, 75);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(379, 205);
+            groupBox2.Size = new Size(401, 205);
             groupBox2.TabIndex = 10;
             groupBox2.TabStop = false;
             groupBox2.Text = "Main Options";
@@ -178,6 +179,18 @@
             chkH.TabIndex = 3;
             chkH.Text = "Copy hidden and system files (/h)";
             chkH.UseVisualStyleBackColor = true;
+            // 
+            // chkDate
+            // 
+            chkDate.AutoSize = true;
+            chkDate.Font = new Font("Segoe UI", 9F, FontStyle.Italic);
+            chkDate.Location = new Point(13, 174);
+            chkDate.Name = "chkDate";
+            chkDate.Size = new Size(194, 24);
+            chkDate.TabIndex = 0;
+            chkDate.Text = "Copy only newer files (/d)";
+            chkDate.UseVisualStyleBackColor = true;
+            chkDate.CheckedChanged += chkDate_CheckedChanged;
             // 
             // chkY
             // 
@@ -217,30 +230,17 @@
             chkS.UseVisualStyleBackColor = true;
             chkS.CheckedChanged += chkS_CheckedChanged;
             // 
-            // chkDate
-            // 
-            chkDate.AutoSize = true;
-            chkDate.Font = new Font("Segoe UI", 9F, FontStyle.Italic);
-            chkDate.Location = new Point(12, 175);
-            chkDate.Name = "chkDate";
-            chkDate.Size = new Size(194, 24);
-            chkDate.TabIndex = 0;
-            chkDate.Text = "Copy only newer files (/d)";
-            chkDate.UseVisualStyleBackColor = true;
-            chkDate.CheckedChanged += chkDate_CheckedChanged;
-            // 
             // groupBox3
             // 
             groupBox3.Controls.Add(dtpDate);
             groupBox3.Controls.Add(chkJ);
-            groupBox3.Controls.Add(chkDate);
             groupBox3.Controls.Add(chkF);
             groupBox3.Controls.Add(chkC);
             groupBox3.Controls.Add(chkK);
             groupBox3.Controls.Add(chkR);
-            groupBox3.Location = new Point(400, 81);
+            groupBox3.Location = new Point(415, 75);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(339, 205);
+            groupBox3.Size = new Size(360, 174);
             groupBox3.TabIndex = 11;
             groupBox3.TabStop = false;
             groupBox3.Text = "Additional Options";
@@ -315,7 +315,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(854, 567);
+            ClientSize = new Size(782, 553);
             Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(btnStop);
